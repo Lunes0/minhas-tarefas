c172f8b258df4420331037188875ec6a50b3c2fc
Lunes0 <lunes6660@gmail.com>
2025-07-28 22:48:26 -0300

    Ajustes iniciais e criação dos primeiros componentes

---
+++ src/containers/BarraLateral/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Aside = styled.aside`
+  padding: 16px;
+  background-color: #eee;
+  height: 100vh;
+`
+
+export const Filtros = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 8px;
+  margin-top: 16px;
+`
+
+export const Input = styled.input`
+  border-radius: 8px;
+  border: 1px solid #ccc;
+  padding: 8px;
+  font-weight: bold;
+  background-color: #fff;
+  color: #666666;
+  border-color: #666666;
+  width: 100%;
+`
